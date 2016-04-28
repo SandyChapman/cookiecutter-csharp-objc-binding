7dc2c73a365070172772ceb2fc46e55340f93407
Sandy Chapman <sandychapman@gmail.com>
2016-04-28 14:08:56 -0400

    Make sure that binding projects properly preserve ObjC code using the LinkerFlags.

--- {{ cookiecutter.repo_name }}/{{ cookiecutter.repo_name }}/lib{{ cookiecutter.library_name }}.linkwith.cs
+++ {{ cookiecutter.repo_name }}/{{ cookiecutter.repo_name }}/lib{{ cookiecutter.library_name }}.linkwith.cs
@@ -1,4 +1,4 @@
 using System;
 using ObjCRuntime;
 
-[assembly: LinkWith ("lib{{ cookiecutter.library_name }}.a", LinkTarget.ArmV7 | LinkTarget.ArmV7s | LinkTarget.Simulator | LinkTarget.Simulator64 | LinkTarget.Arm64, SmartLink = true, ForceLoad = true)]
+[assembly: LinkWith ("lib{{ cookiecutter.library_name }}.a", LinkTarget.ArmV7 | LinkTarget.ArmV7s | LinkTarget.Simulator | LinkTarget.Simulator64 | LinkTarget.Arm64, SmartLink = true, ForceLoad = true, LinkerFlags = "-ObjC")]
